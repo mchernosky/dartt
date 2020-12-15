c5bd81848f73649984d5e388e00fe8ebdbb7d240
mchernosky <matt@chernosky.net>
2020-12-15 15:37:28 -0700

    Clean up graph tests

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,16 +1,20 @@
 require "dartt/graph"
 
+def save_svg(svg, name)
+  File.open("spec/images/#{name}.svg", "w") { |f| f.write(svg) }
+end
+
 RSpec.describe Dartt::Graph do
   it "can create an image" do
     svg = Dartt::Graph.new.render
     expect(svg).to include("<svg")
 
-    File.open("spec/images/blank.svg", "w") { |f| f.write(svg) }
+    save_svg(svg, "blank")
   end
 
   it "can create an image with a rectangle" do
     svg = Dartt::Graph.new.render
 
-    File.open("spec/images/rectangle.svg", "w") { |f| f.write(svg) }
+    save_svg(svg,"rectangle")
   end
 end
