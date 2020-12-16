69f04a6c1748ad945a7ae057085a0c29df7c7c3f
mchernosky <matt@chernosky.net>
2020-12-15 22:20:35 -0700

    Use a viewbox to set the aspect ratio

--- lib/dartt/graph.rb
+++ lib/dartt/graph.rb
@@ -3,8 +3,8 @@ require 'victor'
 module Dartt
   class Graph
     def initialize
-      @svg = Victor::SVG.new
-      @svg.rect x: 0, y: 10, width: 10, height: 10, rx: 5, fill: '#666'
+      @svg = Victor::SVG.new viewBox: '0 0 1920 1080', style: { background: '#ddd' }
+      @svg.rect x: 0, y: 0, width: 1920, height: 1080, rx: 5, fill: '#666'
     end
     def render
       @svg.render
