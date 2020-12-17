3a19ef05a9c0cb71eea6770d25f724aeb7bd4a76
mchernosky <matt@chernosky.net>
2020-12-16 22:06:37 -0700

    Add some boxes

--- lib/dartt/graph.rb
+++ lib/dartt/graph.rb
@@ -3,8 +3,19 @@ require 'victor'
 module Dartt
   class Graph
     def initialize
-      @svg = Victor::SVG.new viewBox: '0 0 1920 1080', style: { background: '#ddd' }
-      @svg.rect x: 0, y: 0, width: 1920, height: 1080, rx: 5, fill: '#666'
+      @width = 1920
+      @height = 1080
+      title_height = 0.1
+      axis_height = 0.1
+      section_width = 0.2
+
+      @svg = Victor::SVG.new viewBox: "0 0 #{@width} #{@height}", style: { background: '#ddd' }
+      # Title
+      @svg.rect x: 0, y: 0, width: @width, height: title_height*@height, rx: 5, fill: '#666'
+      # Graph
+      @svg.rect x: section_width*@width, y: title_height*@height, width: (1-section_width)*@width, height: (1-(title_height + axis_height))*@height, rx: 5, fill: '#999'
+      # Axis
+      @svg.rect x: section_width*@width, y: (1-axis_height)*@height, width: (1-section_width)*@width, height: axis_height*@height, rx: 5, fill: '#666'
     end
     def render
       @svg.render
