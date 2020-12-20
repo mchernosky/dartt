da49f8318e704be05554c0a709ffd23fd076d76c
mchernosky <matt@chernosky.net>
2020-12-19 21:18:50 -0700

    Add area calculations

--- lib/dartt/graph.rb
+++ lib/dartt/graph.rb
@@ -1,24 +1,64 @@
 require 'victor'
 
 module Dartt
+  # A class for calcluating the absolute position from a relative value
+  # within an area defined by absolute position.
+  class Area
+    def initialize(x_start, y_start, x_end, y_end)
+      @x_start = x_start
+      @y_start = y_start
+      @x_end = x_end
+      @y_end = y_end
+      puts "new area (#{x_start},#{y_start},#{x_end},#{y_end})"
+    end
+
+    def w(percent)
+      @x_start + ((@x_end - @x_start)*percent).to_f/100
+    end
+
+    def h(percent)
+      @y_start + (@y_end*percent)/100
+    end
+  end
+
   class Graph
-    def initialize
-      @width = 1920
-      @height = 1080
+    def initialize (width: 1920, height: 1080)
+      @x_end = width
+      @y_end = height
+
       title_height = 0.1
       axis_height = 0.1
       section_width = 0.2
 
-      @svg = Victor::SVG.new viewBox: "0 0 #{@width} #{@height}", style: { background: '#ddd' }
+      @svg = Victor::SVG.new viewBox: "0 0 #{@x_end} #{@y_end}", style: {background: '#ddd' }
       # Title
-      @svg.rect x: 0, y: 0, width: @width, height: title_height*@height, rx: 5, fill: '#666'
+      title = Area.new(0, 0, @x_end, title_height*@y_end)
+      @svg.rect x_start: title.w(0), y_start: title.h(0), x_end: title.w(100), y_end: title.h(100), rx: 5, fill: '#aaa'
+
       # Graph
-      @svg.rect x: section_width*@width, y: title_height*@height, width: (1-section_width)*@width, height: (1-(title_height + axis_height))*@height, rx: 5, fill: '#999'
+      @svg.rect x_start: section_width*@x_end, y_start: title_height*@y_end, x_end: (1-section_width)*@x_end, y_end: (1-(title_height + axis_height))*@y_end, rx: 5, fill: '#999'
       # Axis
-      @svg.rect x: section_width*@width, y: (1-axis_height)*@height, width: (1-section_width)*@width, height: axis_height*@height, rx: 5, fill: '#666'
+      @svg.rect x_start: section_width*@x_end, y_start: (1-axis_height)*@y_end, x_end: (1-section_width)*@x_end, y_end: axis_height*@y_end, rx: 5, fill: '#666'
     end
     def render
       @svg.render
+    end
+
+    def w(percent)
+      @active_area.w(percent)
+    end
+
+    def area(x, y, width, height, &block)
+      # Convert from percentage coordinates to absolute coordinates for
+      # creating the area.
+      x = x*@x_end/100
+      width = width*@x_end/100
+      y = y*@y_end/100
+      height = height*@y_end/100
+      @active_area = Area.new(x,y,width,height)
+      if block_given?
+        instance_eval(&block)
+      end
     end
   end
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -5,6 +5,7 @@ def save_svg(svg, name)
 end
 
 RSpec.describe Dartt::Graph do
+
   it "can create an image" do
     svg = Dartt::Graph.new.render
     expect(svg).to include("<svg")
@@ -17,4 +18,86 @@ RSpec.describe Dartt::Graph do
 
     save_svg(svg,"rectangle")
   end
+
+  it "can define a new area" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    graph.area(0,0,100,100)
+  end
+
+  it "can get the beginning width of the area" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(0,0,100,100) do
+      w 0
+    end
+    expect(x).to eq(0)
+  end
+
+  it "can get the ending width of the area" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(0,0,100,100) do
+      w 100
+    end
+    expect(x).to eq(1920)
+  end
+
+  it "can get the beginning width of the area with offset" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(10,0,100,100) do
+      w 0
+    end
+    expect(x).to eq(192)
+  end
+
+  it "can get the ending width of the area with offset" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(10,0,100,100) do
+      w 100
+    end
+    expect(x).to eq(1920)
+  end
+
+  it "can get the ending width of the area with margins" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(10,0,90,100) do
+      w 100
+    end
+    expect(x).to eq(1728)
+  end
+
+  it "can get some width in the middle of an area" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(10,0,20,100) do
+      w 50
+    end
+    expect(x).to eq(288)
+  end
+
+  it "can get some other width in the middle of an area" do
+    graph = Dartt::Graph.new(width: 1920, height: 1080)
+    x = graph.area(10,0,20,100) do
+      w 40
+    end
+    expect(x).to eq(268.8)
+  end
+
+  # it "can define another area" do
+  #   graph = Dartt::Graph.new(width: 1920, height: 1080)
+  #   graph.area(0,0,100,100) do
+  #     expect(w 50).to eq(960)
+  #   end
+  # end
+  #
+  # it "can define another area" do
+  #   graph = Dartt::Graph.new(width: 1920, height: 1080)
+  #   graph.area(0,0,50,50) do
+  #     expect(w 50).to eq(480)
+  #   end
+  # end
+  #
+  # it "can define another area a" do
+  #   graph = Dartt::Graph.new(width: 1920, height: 1080)
+  #   graph.area(0,0,10,100) do
+  #     expect(w 50).to eq(96)
+  #   end
+  # end
 end
