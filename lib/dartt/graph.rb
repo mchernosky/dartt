f78c87f84a13292d3475078aff8e3a8f62778f9d
Matt <matt@electronvector.com>
2020-12-22 08:30:07 -0700

    Calculate area sizes

--- lib/dartt/graph.rb
+++ lib/dartt/graph.rb
@@ -5,13 +5,14 @@ module Dartt
 
   class Graph < Victor::SVG
     def initialize (title, width: 1920, height: 1080)
+      @title = title
+      @width = width
+      @height = height
 
       title_height = 10
       axis_height = 10
       section_width = 20
-      @title = title
-      @width = width
-      @height = height
+
       super viewBox: "0 0 #{@width} #{@height}", font_family: 'arial', font_size: 40, fill: "white", text_anchor:"middle", dominant_baseline:"middle"
       svg x:"0%", y:"0%", width:"100%", height:"10%" do
         rect width: "100%", height: "100%", rx: 10, fill: '#999'
@@ -19,19 +20,19 @@ module Dartt
       end
 
       # Sections
-      svg x:"0%", y:"10%", width:"25%", height:"90%" do
+      svg x:"0%", y:"#{title_height}%", width:"#{section_width}%", height:"#{100-title_height}%" do
         rect width:"100%", height:"100%", rx: 10, fill: '#666'
         text "Sections", x: "50%", y: "50%"
       end
 
         # Graph
-      svg x:"25%", y:"10%", width:"75%", height:"80%" do
+      svg x:"#{section_width}%", y:"#{title_height}%", width:"#{100-section_width}%", height:"#{100-title_height-axis_height}%" do
         rect width:"100%", height:"100%", rx: 10, fill: '#aaa'
         text "Graph", x: "50%", y: "50%"
       end
 
       # Axis
-      svg x:"25%", y:"90%", width:"75%", height:"10%" do
+      svg x:"#{section_width}%", y:"#{100-title_height}%", width:"#{100-section_width}%", height:"#{title_height}%" do
         rect width:"100%", height:"100%", rx: 10, fill: 'blue'
         text "Axis", x: "50%", y: "50%"
       end
