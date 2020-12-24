51cc13278c9d221819b1701e1353942048c776cb
Matt <matt@electronvector.com>
2020-12-24 10:47:42 -0700

    Refactor

--- lib/dartt/graph.rb
+++ lib/dartt/graph.rb
@@ -12,14 +12,17 @@ module Dartt
 
       @day_width = 100.0/@total_days
 
+      # TODO: Turn these into config options in some hash/yaml.
       title_height = 10
       axis_height = 10
       section_width = 20
 
+      # Draw the parent SVG.
       super viewBox: "0 0 #{@width} #{@height}", font_family: 'arial', font_size: 40, fill: "white", text_anchor:"middle", dominant_baseline:"middle"
+
+      # Draw the title.
       svg x:"0%", y:"0%", width:"100%", height:"10%" do
-        rect width: "100%", height: "100%", rx: 10, fill: '#999'
-        text @title, x: "50%", y: "50%"
+        text @title, x: "50%", y: "50%", fill: "black"
       end
 
       # Sections
@@ -36,10 +39,10 @@ module Dartt
           line x1:"#{@day_width*day}%", y1:"0%", x2:"#{@day_width*day}%", y2:"100%", stroke:"#666"
         end
         # Create some sample task bars.
-        draw_task("Planning2", 0, 1, 3, @day_width, 'blue')
-        draw_task("Demo2", 1, 5, 2, @day_width, 'red')
-        draw_task("Integration", 2, 7, 5, @day_width, "green")
-        draw_task("Planning", 3, 1, 10, @day_width, "blue")
+        draw_task("Planning2", 0, 1, 3)
+        draw_task("Demo2", 1, 5, 2)
+        draw_task("Integration", 2, 7, 5)
+        draw_task("Planning", 3, 1, 10)
       end
 
       # Axis
@@ -51,10 +54,10 @@ module Dartt
 
     private
 
-    def draw_task(name, row, start_day, duration, x_spacing, color)
+    def draw_task(name, row, start_day, duration)
       task_height = 50
-      rect x: "#{(start_day - 1) * x_spacing}%", y: row * task_height, width: "#{duration * x_spacing}%", height: task_height, fill: color, rx: 5
-      text name, x: "#{(start_day - 1) * x_spacing + duration * x_spacing / 2}%", y: row * task_height + (task_height / 2), font_size: 24
+      rect x: "#{(start_day - 1) * @day_width}%", y: row * task_height, width: "#{duration * @day_width}%", height: task_height, fill: "green", rx: 5
+      text name, x: "#{(start_day - 1) * @day_width + duration * @day_width / 2}%", y: row * task_height + (task_height / 2), font_size: 24
     end
 
   end
