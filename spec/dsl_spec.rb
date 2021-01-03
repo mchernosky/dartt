06101750f78fa0bec56e3b32c409090751da766c
Matt <matt@electronvector.com>
2021-01-02 18:40:47 -0700

    Add more tests of added and not added tasks to chart

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -13,10 +13,27 @@ RSpec.describe Dartt::Chart do
     expect(c.name).to eq("A Chart")
   end
 
-  it "can add tasks to a chart" do
+  it "can add a task to a chart" do
     c = Dartt.chart "Three Month Project" do
       task "A Task"
     end
     expect(c).to include(Dartt::Task.new("A Task"))
+  end
+
+  it "can add multiple tasks to a chart" do
+    c = Dartt.chart "Three Month Project" do
+      task "A Task"
+      task "Another Task"
+    end
+    expect(c).to include(Dartt::Task.new("A Task"))
+    expect(c).to include(Dartt::Task.new("Another Task"))
+  end
+
+  it "doesn't have tasks that have not been added" do
+    c = Dartt.chart "Three Month Project" do
+      task "A Task"
+    end
+    expect(c).to include(Dartt::Task.new("A Task"))
+    expect(c).not_to include(Dartt::Task.new("Task Not Added"))
   end
 end
