e5bc33ab09b7f92be9d4450788ad3332ff7e9264
Matt <matt@electronvector.com>
2021-01-04 10:48:33 -0700

    Add a note to fix something later

--- lib/dartt/example_chart.rb
+++ lib/dartt/example_chart.rb
@@ -8,8 +8,12 @@ def save_svg(svg, name)
 end
 
 c = Dartt.chart "The Schedule", Date.new(2021, 1, 4), Date.new(2021, 3, 24) do
-  kickoff = milestone "Kickoff Unblocked", Date.new(2021, 1, 4)
-  task "Register definition review", after: kickoff, duration: 5
+  # TODO: Fix that we can't start a task after a milestone on a monday.
+  kickoff = milestone "Kickoff Unblocked", Date.new(2021, 1, 3)
+  t = task "Register definition review", after: kickoff, duration: 5
+  puts kickoff.start
+  puts kickoff.end
+  puts t.start
 end
 
 save_svg(c.render, "example_chart")
