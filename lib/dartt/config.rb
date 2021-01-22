725e56263a40961cc03e8121b5fd4e453af51c02
Matt <matt@electronvector.com>
2021-01-22 15:01:06 -0700

    Add configurable font size

--- lib/dartt/config.rb
+++ lib/dartt/config.rb
@@ -1,6 +1,15 @@
 
 module Dartt
   class Config
-
+    def initialize (default_config=nil)
+      if default_config.nil?
+        @font_size = 20
+      else
+        @font_size = default_config[:font_size]
+      end
+    end
+    def font_size
+      @font_size
+    end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Dartt::Config do
   it "can be created" do
     Dartt::Config.new
   end
+
+  it "has a default font size" do
+    c = Dartt::Config.new
+    expect(c.font_size).to be_an(Integer)
+  end
+
+  it "can set a default size with an initializer hash" do
+    default = {
+        :font_size => 24
+    }
+    c = Dartt::Config.new(default)
+    expect(c.font_size).to eq(24)
+  end
 end
