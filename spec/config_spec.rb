12a392c6a8bacc835f53c7ef704d4cb4c1becdbf
mchernosky <matt@chernosky.net>
2021-01-23 21:29:40 -0700

    Override title height

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -10,12 +10,23 @@ RSpec.describe Dartt::Config do
     expect(c[:width]).to be_an(Integer)
   end
 
-  it "the default width can be overridden with an initializer hash" do
+  it "can override the default width with an initializer hash" do
     c = Dartt::Config.build(
         {
             :width => 24
         }
     )
     expect(c[:width]).to eq(24)
+  end
+
+  it "can override the title heigth with an initializer hash" do
+    c = Dartt::Config.build(
+        {
+            :title => {
+                :height => 500
+            }
+        }
+    )
+    expect(c[:title][:height]).to eq(500)
   end
 end
